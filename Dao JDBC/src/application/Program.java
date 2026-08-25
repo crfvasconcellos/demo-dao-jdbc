8af185eeed96fa82e2c222c4d3f6fca5847c7c36
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-08-25 13:00:38 -0300

    Department entity class

---
+++ Dao JDBC/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+    static void main() {
+
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
+
+
+
+    }
+}
