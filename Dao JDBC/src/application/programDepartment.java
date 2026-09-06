cf1ae483055790ecd3a500608ba2cb8891f8eade
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-09-06 13:32:39 -0300

    FindByID and Update Department Implementation

--- Dao JDBC/src/application/programDepartment.java
+++ Dao JDBC/src/application/programDepartment.java
@@ -9,17 +9,27 @@ public class programDepartment {
 
         DepartmentDao departmentDao = DaoFactory.CreateDepartmentDao();
 
-        System.out.println("=== Test 1: Department Insert ===");
-        Department newDepartment = new Department(null,"Law");
-        departmentDao.insert(newDepartment);
-        if (newDepartment.getId() != null){
-            System.out.println("Inserted New ID = " + newDepartment.getId());
-            System.out.println(newDepartment);
-        }
+//        System.out.println("=== Test 1: Department Insert ===");
+//        Department newDepartment = new Department(null,"Law");
+//        departmentDao.insert(newDepartment);
+//        if (newDepartment.getId() != null){
+//            System.out.println("Inserted New ID = " + newDepartment.getId());
+//            System.out.println(newDepartment);
+//        }
+//
+//        System.out.println("=== Test 2: Department DeletById ===");
+//        departmentDao.deleteById(6);
+//        System.out.println("Deletd ID");
 
-        System.out.println("=== Test 2: Department DeletById ===");
-        departmentDao.deleteById(6);
-        System.out.println("Deletd ID");
+        System.out.println("=== Test 3: Department FindById ===");
+        Department dep = departmentDao.findbyId(4);
+        System.out.println(dep);
 
+        System.out.println("=== Test 4: Department update ===");
+        Department dep2 = departmentDao.findbyId(4);
+        System.out.println("Before: " + dep2);
+        dep2.setName("Clothes");
+        departmentDao.update(dep2);
+        System.out.println("After: " + dep2);
     }
 }
--- Dao JDBC/src/model/dao/impl/DepartmentDaoJDBC.java
+++ Dao JDBC/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -45,7 +45,20 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?");
+            st.setString(1,obj.getName());
+            st.setInt(2,obj.getId());
 
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -66,7 +79,27 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findbyId(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT id, name FROM department WHERE id = ? ");
+            st.setInt(1,id);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+                Department department = new Department(rs.getInt("Id"),rs.getString("Name"));
+                return department;
+            }
         return null;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
