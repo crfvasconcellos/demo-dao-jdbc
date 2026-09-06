69a95474a9679bfe1a6c81457fc88c072f409160
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-09-06 12:09:32 -0300

    Insert Department Implementation

---
+++ Dao JDBC/src/application/programDepartment.java
@@ -0,0 +1,21 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class programDepartment {
+    static void main() {
+
+        DepartmentDao departmentDao = DaoFactory.CreateDepartmentDao();
+
+        System.out.println("=== Test 1: Department Insert ===");
+        Department newDepartment = new Department(null,"Law");
+        departmentDao.insert(newDepartment);
+        if (newDepartment.getId() != null){
+            System.out.println("Inserted New ID = " + newDepartment.getId());
+            System.out.println(newDepartment);
+        }
+
+    }
+}
--- Dao JDBC/src/model/dao/DaoFactory.java
+++ Dao JDBC/src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,4 +9,9 @@ public class DaoFactory {
     public static SellerDao CreateSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
     }
+
+    public static DepartmentDao CreateDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
+    }
+
 }
