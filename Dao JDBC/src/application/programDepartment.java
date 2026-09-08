b0660baf796b2f062b2de028f494a71bebfce504
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-09-08 12:37:00 -0300

    I removed the comments

--- Dao JDBC/src/application/programDepartment.java
+++ Dao JDBC/src/application/programDepartment.java
@@ -9,17 +9,17 @@ public class programDepartment {
 
         DepartmentDao departmentDao = DaoFactory.CreateDepartmentDao();
 
-//        System.out.println("=== Test 1: Department Insert ===");
-//        Department newDepartment = new Department(null,"Law");
-//        departmentDao.insert(newDepartment);
-//        if (newDepartment.getId() != null){
-//            System.out.println("Inserted New ID = " + newDepartment.getId());
-//            System.out.println(newDepartment);
-//        }
-//
-//        System.out.println("=== Test 2: Department DeletById ===");
-//        departmentDao.deleteById(6);
-//        System.out.println("Deletd ID");
+        System.out.println("=== Test 1: Department Insert ===");
+        Department newDepartment = new Department(null,"Law");
+        departmentDao.insert(newDepartment);
+        if (newDepartment.getId() != null){
+            System.out.println("Inserted New ID = " + newDepartment.getId());
+            System.out.println(newDepartment);
+        }
+
+        System.out.println("=== Test 2: Department DeletById ===");
+        departmentDao.deleteById(6);
+        System.out.println("Deletd ID");
 
         System.out.println("=== Test 3: Department FindById ===");
         Department dep = departmentDao.findbyId(4);
